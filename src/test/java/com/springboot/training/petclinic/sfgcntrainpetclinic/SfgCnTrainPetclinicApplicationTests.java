1e054a41146f697c134159b3f955abb0c510e827
cnoriega20 <c79.noriega@gmail.com>
2020-08-17 18:43:02 -0500

    Initial Project structure

---
+++ src/test/java/com/springboot/training/petclinic/sfgcntrainpetclinic/SfgCnTrainPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.training.petclinic.sfgcntrainpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgCnTrainPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

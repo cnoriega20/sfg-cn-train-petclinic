e24ea7d1f581d6b0bf21271070a80d164bff367a
cnoriega20 <c79.noriega@gmail.com>
2020-09-14 14:28:31 -0500

    Maven multi-module configuration.

--- sfg-cn-petclinic-web/src/main/java/sfgcntrainpetclinicweb/SfgCnTrainPetclinicApplication.java
+++ sfg-cn-petclinic-web/src/main/java/sfgcntrainpetclinicweb/SfgCnTrainPetclinicApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.training.petclinic.sfgcntrainpetclinic;
+package sfgcntrainpetclinicweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- sfg-cn-petclinic-web/src/test/java/sfgcntrainpetclinicweb/SfgCnTrainPetclinicApplicationTests.java
+++ sfg-cn-petclinic-web/src/test/java/sfgcntrainpetclinicweb/SfgCnTrainPetclinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springboot.training.petclinic.sfgcntrainpetclinic;
+package sfgcntrainpetclinicweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

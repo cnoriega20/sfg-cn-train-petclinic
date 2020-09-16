7ea438c26f642d69bfa2923d943741d3748b3a38
cnoriega20 <c79.noriega@gmail.com>
2020-09-16 14:34:57 -0500

    Added Owner index page and controller.

---
+++ sfg-cn-petclinic-web/src/main/java/sfgcntrainpetclinicweb/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package sfgcntrainpetclinicweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owners", "owners/index", "owners/index.html"})
+    public String getListOfVets(){
+        return "owners/index";
+    }
+}

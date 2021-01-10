6dbbe7b581021cf546eb54d48f5ed1a8601d6830
cnoriega20 <c79.noriega@gmail.com>
2021-01-10 15:33:32 -0600

    Add Logic of List of Owners.

--- sfg-cn-petclinic-web/src/main/java/sfgcntrainpetclinicweb/controllers/OwnerController.java
+++ sfg-cn-petclinic-web/src/main/java/sfgcntrainpetclinicweb/controllers/OwnerController.java
@@ -1,12 +1,22 @@
 package sfgcntrainpetclinicweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfgcntrainpetclinicweb.sfgcntrainpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "owners/index", "owners/index.html"})
-    public String getListOfVets(){
+    public String getListOfOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }

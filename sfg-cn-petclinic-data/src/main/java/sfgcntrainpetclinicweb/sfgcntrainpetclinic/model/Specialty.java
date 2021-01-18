a458811ce73b4efe6c0e6afa6cc6e3aa0307fa9f
cnoriega20 <c79.noriega@gmail.com>
2021-01-18 13:03:40 -0600

    Addimg Specialty pojo and enhanced Vet Pojo with Specialty

--- sfg-cn-petclinic-data/src/main/java/sfgcntrainpetclinicweb/sfgcntrainpetclinic/model/Specialty.java
+++ sfg-cn-petclinic-data/src/main/java/sfgcntrainpetclinicweb/sfgcntrainpetclinic/model/Specialty.java
@@ -5,12 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Vet extends Person {
-    private Set<Specialty> specialties;
+public class Specialty extends BaseEntity{
+    private String description;
 }

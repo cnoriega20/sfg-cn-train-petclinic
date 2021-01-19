8415405b436630d56a99b11c62da5a48c38ebac4
cnoriega20 <c79.noriega@gmail.com>
2021-01-18 20:00:03 -0600

    Added SpecialtiesMapService.

---
+++ sfg-cn-petclinic-data/src/main/java/sfgcntrainpetclinicweb/sfgcntrainpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package sfgcntrainpetclinicweb.sfgcntrainpetclinic.services;
+
+import sfgcntrainpetclinicweb.sfgcntrainpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends PetClinicCrudService<Specialty, Long> {
+}

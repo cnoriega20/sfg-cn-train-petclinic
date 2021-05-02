c8694d790f59314488d07756e939532613150c10
cnoriega20 <c79.noriega@gmail.com>
2021-05-02 09:17:47 -0500

    Added  Map Based Visit Service.

---
+++ sfg-cn-petclinic-data/src/main/java/sfgcntrainpetclinicweb/sfgcntrainpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package sfgcntrainpetclinicweb.sfgcntrainpetclinic.services;
+
+import sfgcntrainpetclinicweb.sfgcntrainpetclinic.model.Visit;
+
+public interface VisitService extends PetClinicCrudService<Visit, Long>{
+}
